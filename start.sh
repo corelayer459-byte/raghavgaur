bbb85675dbffacb4c9986ea068dcfe89c5ef8c90
corelayer459-byte <corelayer459@gmail.com>
2026-03-18 09:19:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Enable password authentication
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
 # Set root password
 echo "root:mypassword123" | chpasswd
 
-# Start SSH server
+# Generate SSH keys
+ssh-keygen -A
+
+# Start SSH
 service ssh start
 
 # Keep alive 24/7
